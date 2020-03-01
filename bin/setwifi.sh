c24103c4dc049b13ed583788f3b4ab92b41d4fd2
Zorglub42 <contact@zorglub42.fr>
2020-03-01 07:48:12 +0100

    [FIX] SetIP

--- bin/setwifi.sh
+++ bin/setwifi.sh
@@ -125,4 +125,13 @@ if [ "$MODE" == "client" ] ; then
 else
 	createHotspot
 fi
+ifconfig | grep tap0 > /dev/null
+if [ $? -eq 0 ] ; then
+        IF=tap0
+else
+        IF=wlan0
+fi
+IP=$(ifconfig $IF | grep "inet " | awk '{print $2}')
+echo "IP $IP" | nc localhost 6942
+
 exit 0
